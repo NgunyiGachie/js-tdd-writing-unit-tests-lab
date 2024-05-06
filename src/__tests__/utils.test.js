d73220bf28b83491b701d114b57ec53373e8c356
NgunyiGachie <antogachie@gmail.com>
2024-05-06 05:11:44 +0300

    Complete lab

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,1 +1,16 @@
-// Your tests here
+import { isPalindrome } from "../utils"
+
+describe("isPalindrome", () => {
+    it("returns true for a palindrome", () => {
+        expect(isPalindrome("racecar")).toBe(true);
+    });
+    it("returns false for a non-palindrome", () => {
+        expect(isPalindrome("car")).toBe(false);
+    })
+    it("returns true for a palindrome with mixed case", () => {
+        expect(isPalindrome("RaCeCaR")).toBe(true);
+    });
+    it("throws an error if input is not a string", () => {
+        expect(() => isPalindrome(123)).toThrow("Input must be a string")
+    });
+})
--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,14 @@
-// Your code here
+export function isPalindrome(string) {
+    if (typeof string !== 'string') {
+        throw new Error("Input must be a string");
+    }
+    const len = string.length;
+    for (let i=0; i< len / 2; i++) {
+        if (string[i] !== string[len -1 -i]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+
